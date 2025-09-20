9ae5c4f87bbbdc97ef2e8c9374f37439ba751c0c
Hamza El Asli <hel-asli@e2r13p4.1337.ma>
2025-09-20 02:10:36 +0100

    Add Plugin

--- lua/config/init.lua
+++ lua/config/init.lua
@@ -2,3 +2,4 @@ vim.g.mapleader = " "
 
 require("config.lazy_conf")
 require("config.set")
+require("config.remap")
---
+++ lua/config/remap.lua
@@ -0,0 +1,5 @@
+vim.keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>f", ":NvimTreeFocus<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>c", ":NvimTreeClose<CR>", { noremap = true, silent = true })
+
+
