667524a0a0f92fe48290b416f48a08b244869e21
hel-asli <helasli528@gmail.com>
2025-09-20 13:22:33 +0100

    neovim gruvbox theme

--- lua/config/set.lua
+++ lua/config/set.lua
@@ -1,5 +1,11 @@
 -- setup must be called before loading
-vim.cmd.colorscheme "catppuccin"
+vim.cmd.colorscheme "gruvbox"
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.shiftwidth = 4
+vim.opt.tabstop = 4
+vim.opt.smartindent = true
+vim.opt.termguicolors = true
 vim.cmd [[
   hi Normal guibg=NONE ctermbg=NONE
   hi NormalFloat guibg=NONE ctermbg=NONE
