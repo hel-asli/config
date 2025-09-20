9fb0c3e72b60b165198479892c76b122faa96185
Hamza El Asli <hel-asli@e2r13p4.1337.ma>
2025-09-20 03:40:55 +0100

    nvim term plugin

--- lua/config/remap.lua
+++ lua/config/remap.lua
@@ -2,4 +2,8 @@ vim.keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>", { noremap = true, silent
 vim.keymap.set("n", "<leader>f", ":NvimTreeFocus<CR>", { noremap = true, silent = true })
 vim.keymap.set("n", "<leader>c", ":NvimTreeClose<CR>", { noremap = true, silent = true })
 
+-- Terminal keymaps
+vim.keymap.set("n", "<leader>t", ":ToggleTerm<CR>", { noremap = true, silent = true })
+vim.keymap.set("t", "<Esc>", "<C-\><C-n>", { noremap = true, silent = true })
+
 
