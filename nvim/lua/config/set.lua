b48ac630abd79a549591ac39f49e96a6f82a67e9
Hamza El Asli <hel-asli@e2r1p1.1337.ma>
2025-09-23 09:28:34 +0100

    theme config

--- nvim/lua/config/set.lua
+++ nvim/lua/config/set.lua
@@ -1,13 +1,9 @@
 -- setup must be called before loading
-vim.cmd.colorscheme "gruvbox"
+vim.cmd.colorscheme "catppuccin-macchiato"
 vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.shiftwidth = 4
 vim.opt.tabstop = 4
 vim.opt.smartindent = true
 vim.opt.termguicolors = true
-vim.cmd [[
-  hi Normal guibg=NONE ctermbg=NONE
-  hi NormalFloat guibg=NONE ctermbg=NONE
-]]
 
